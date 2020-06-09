da0b11d1c3ce83f77d4527d30b427323b390acd0
Anestis Finstad <munkeyten@protonmail.com>
2020-06-10 00:34:53 +0200

    Added checkout item
    Added actions addItem and removeItem to checkout item.

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -12,14 +12,15 @@ import './cart-icon.styles.scss';
 const CartIcon = ({ toggleCartHidden, itemCount }) => (
     <div className="cart-icon" onClick={toggleCartHidden}>
         <ShoppingIcon className="shopping-icon" />
-<span className="item-count">{itemCount}</span>
+        <span className="item-count">{itemCount}</span>
     </div>
 )
+
+
 
 const mapDispatchToProps = dispatch => ({
     toggleCartHidden: () => dispatch(toggleCartHidden())
 })
-
 
 // Memoization is the caching of the selectors value, which can be done using a library called re-select.
 const mapStateToProps = createStructuredSelector({
--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -16,5 +16,23 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
             : cartItem
         )
     }
-    return [...cartItems, { ...cartItemToAdd, quantity: 1}]
-}
+    return [...cartItems, { ...cartItemToAdd, quantity: 1 }]
+};
+
+// Removes items from cart
+export const removeItemFromCart = (cartItems, cartItemToRemove) => {
+    const existingCartItem = cartItems.find(
+        cartItem => cartItem.id === cartItemToRemove.id
+    );
+
+    if(existingCartItem.quantity === 1) {
+        return cartItems.filter(cartItem => cartItem.id !== cartItemToRemove.id)
+    }
+
+    return cartItems.map(
+        cartItem => 
+        cartItem.id === cartItemToRemove.id ?
+        { ...cartItem, quantity: cartItem.quantity - 1 }
+        : cartItem
+    );
+};
